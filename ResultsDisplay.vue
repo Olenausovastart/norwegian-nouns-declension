e0f228c1f154c6eb592e69982461d3ccb2d17983
Olenausovastart <159901412+Olenausovastart@users.noreply.github.com>
2024-02-13 12:07:27 +0100

    Create ResultsDisplay.vue

---
+++ ResultsDisplay.vue
@@ -0,0 +1,15 @@
+<!-- ResultsDisplay.vue -->
+<template>
+  <div v-if="results.length > 0">
+    <h2>Results</h2>
+    <ul>
+      <li v-for="result in results" :key="result.id">{{ result }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['results']
+};
+</script>
