00c9837abdec0f8c3ecaaff18af526530236a7c7
Olenausovastart <159901412+Olenausovastart@users.noreply.github.com>
2024-02-13 12:05:27 +0100

    Create TextInput.vue

---
+++ TextInput.vue
@@ -0,0 +1,19 @@
+<!-- TextInput.vue -->
+<template>
+  <input type="text" v-model="inputValue" @input="handleInput">
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: ''
+    };
+  },
+  methods: {
+    handleInput(event) {
+      this.inputValue = event.target.value;
+    }
+  }
+};
+</script>
