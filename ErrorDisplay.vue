06375eb17f879d2eb287ec5ba43f2b299c4c8c4a
Olenausovastart <159901412+Olenausovastart@users.noreply.github.com>
2024-02-13 12:08:23 +0100

    Create ErrorDisplay.vue

---
+++ ErrorDisplay.vue
@@ -0,0 +1,15 @@
+<!-- ErrorDisplay.vue -->
+<template>
+  <div v-if="errors.length > 0">
+    <h2>Error</h2>
+    <ul>
+      <li v-for="error in errors" :key="error.id">{{ error }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['errors']
+};
+</script>
